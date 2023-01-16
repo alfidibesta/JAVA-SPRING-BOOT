663dd1e0c361d1c5bca96c3899659178541572b5
Besta Alfidi <alfidibesta@gmail.com>
2023-01-16 09:49:56 +0700

    update new project learning

---
+++ learning/src/test/java/spring/java/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.java.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
